5d33519fdcfeb5c208b02ea0613d0227f58413b9
vladislav <vlad.kovalenko@aviasales.ru>
2021-01-09 19:31:50 +0700

    tables structure

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -15,6 +15,72 @@ CREATE TABLE flags(
 
   UNIQUE(key)
 );
-
 CREATE INDEX ON flags(deleted_at);
+
+CREATE TABLE tags(
+  id serial primary key,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  value varchar(64)
+);
+CREATE INDEX on tags(value);
+CREATE INDEX on tags(deleted_at);
+
+CREATE TABLE flags_tags(
+  flag_id integer not null references flags(id),
+  tag_id integer not null references tags(id),
+
+  primary key(flag_id, tag_id)
+);
+
+CREATE TABLE variants(
+  id serial primary key,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  flag_id INTEGER NOT NULL REFERENCES flags(id),
+  key text,
+  attachment text
+);
+
+CREATE TABLE segments(
+  id serial primary key,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  flag_id INTEGER NOT NULL REFERENCES flags(id),
+  description text,
+  rank integer,
+  rollout_ppm integer
+);
+CREATE INDEX on segments(flag_id);
+CREATE INDEX on segments(deleted_at);
+
+CREATE TABLE distributions(
+  id serial primary key,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  segment_id INTEGER NOT NULL REFERENCES segments(id),
+  variant_id INTEGER NOT NULL REFERENCES variants(id),
+  ppm INTEGER NOT NULL,
+  bitmap TEXT
+);
+CREATE INDEX on distributions(deleted_at);
+CREATE INDEX on distributions(segment_id);
+CREATE INDEX on distributions(variant_id);
+
+CREATE TABLE constraints(
+  id serial primary key,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default (current_timestamp at time zone 'UTC'),
+  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  segment_id INTEGER NOT NULL REFERENCES segments(id),
+  property text,
+  operator text,
+  value text
+);
+CREATE INDEX on constraints(deleted_at);
+CREATE INDEX on constraints(segment_id);
 
---
+++ src/test/scala/sigil/repo/impl/pg/FlagRepoPGImplSpec.scala
@@ -0,0 +1,5 @@
+package sigil.repo.impl.pg
+
+import org.scalatest.freespec.AnyFreeSpec
+
+class FlagRepoPGImplSpec extends AnyFreeSpec {}
