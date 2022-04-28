fb0d8b52c833f897cb50ae9894270e9883dcfe81
vladislav <vlad1997kov@gmail.com>
2022-04-28 23:41:35 +0200

    add constraints operators

--- src/main/scala/sigil/model/Constraint.scala
+++ src/main/scala/sigil/model/Constraint.scala
@@ -6,3 +6,38 @@ final case class Constraint(
   operator: String,
   value: String
 )
+
+object Constraint {
+
+  sealed trait Operator
+  case object EQ extends Operator
+  case object NEQ extends Operator
+  case object LT extends Operator
+  case object LTE extends Operator
+  case object GT extends Operator
+  case object GTE extends Operator
+  case object EREG extends Operator
+  case object NEREG extends Operator
+  case object IN extends Operator
+  case object NOTIN extends Operator
+  case object CONTAINS extends Operator
+  case object NOTCONTAINS extends Operator
+
+  object Operator {
+    def fromString(str: String): Option[Operator] = str match {
+      case "=="           => Some(EQ)
+      case "!="           => Some(NEQ)
+      case "<"            => Some(LT)
+      case "<="           => Some(LTE)
+      case ">"            => Some(GT)
+      case ">="           => Some(GTE)
+      case "=~"           => Some(EREG)
+      case "!~"           => Some(NEREG)
+      case "IN"           => Some(IN)
+      case "NOT IN"       => Some(NOTIN)
+      case "CONTAINS"     => Some(CONTAINS)
+      case "NOT CONTAINS" => Some(NOTCONTAINS)
+      case _              => None
+    }
+  }
+}
